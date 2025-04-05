9a8a0e249129033ac333055b41f5e104f66065be
Sreelal <sreelalat2@gmail.com>
2025-04-05 22:38:54 +0530

    fix: monthly routes issue resolved

--- src/routes/monthlyRoutes.ts
+++ src/routes/monthlyRoutes.ts
@@ -6,7 +6,17 @@ const router = express.Router();
 //Get Monthly summary
  router.get('/', async (_req, res)=>{
      const summaries = await MonthlySummary.find().populate("userId", "name phoneNumber");
-     res.json(summaries);
+     const formattedData = summaries.map((item: any) => ({
+       _id: item._id,
+       month: item.month,
+       userId: item.userId?._id,
+       name: item.userId?.name,
+       phoneNumber: item.userId?.phoneNumber,
+       totalAmount: item.totalAmount,
+       totalOptOutDays: item.totalOptOutDays,
+       __v: item.__v,
+     }));
+     res.json(formattedData);
  })
 
  export default router;
